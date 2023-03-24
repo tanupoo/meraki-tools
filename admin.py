506119d18b0cae91ce71da04693e373a4a1a4199
tanupoo <github-shoichi@tanu.org>
2023-03-24 23:26:37 +0900

    supported asyncio and as default.

--- admin.py
+++ admin.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import argparse
-from meraki_api import *
 import json
 
 def get_conf(config: str) -> dict:
@@ -25,22 +24,35 @@ ap.add_argument("--network-id", "-N", action="store", dest="network_id",
                 help="specify the network ID.")
 ap.add_argument("-k", "--apikey", action="store", dest="api_key",
                 help=f"specify the API key with the prefix of either 'file:' or 'key:'.")
+ap.add_argument("--no-async", action="store_true", dest="non_async",
+                help=f"specify not to use asyncio")
+ap.add_argument("-d", action="store_true", dest="debug",
+                help=f"enable debug mode")
 ap.add_argument("config",
                 help=f"specify the config filename.")
 opt = ap.parse_args()
 
 config = get_conf(opt.config)
+if opt.non_async:
+    from meraki_api import meraki_api
+    meraki = meraki_api()
+else:
+    from meraki_asyncio_api import meraki_asyncio_api
+    from asyncio import new_event_loop
+    meraki = meraki_asyncio_api(new_event_loop())
+
+meraki.set_apikey(api_key_spec=opt.api_key,
+                  config_api_key_spec=config["api_key_spec"])
+
 if opt.get_orgs:
-    meraki_set_apikey(api_key_spec=opt.api_key, config=config)
-    ret = meraki_get_all_orgs()
+    ret = meraki.get_all_orgs(debug=opt.debug)
     for a in ret:
         print(f"## name:{a['name']} id:{a['id']}")
         print(a)
         print()
 elif opt.get_networks:
     if opt.org_id:
-        meraki_set_apikey(api_key_spec=opt.api_key, config=config)
-        ret = meraki_get_all_networks(opt.org_id)
+        ret = meraki.get_all_networks(opt.org_id, debug=opt.debug)
         for a in ret:
             print(f"## name:{a['name']} id:{a['id']}")
             print(a)
@@ -50,8 +62,7 @@ elif opt.get_networks:
         exit(1)
 elif opt.get_ssids:
     if opt.network_id:
-        meraki_set_apikey(api_key_spec=opt.api_key, config=config)
-        ret = meraki_get_all_ssids(opt.network_id)
+        ret = meraki.get_all_ssids(opt.network_id, debug=opt.debug)
         for a in ret:
             print(f"## name:{a['name']} enabled:{a['enabled']}")
             print(a)
@@ -59,10 +70,9 @@ elif opt.get_ssids:
     else:
         print("ERROR: specify the network id")
         exit(1)
-elif opt.get_ssids:
+elif opt.get_sensors:
     if opt.network_id:
-        meraki_set_apikey(api_key_spec=opt.api_key, config=config)
-        ret = meraki_sensors_get_metric(opt.network_id)
+        ret = meraki.sensors_get_metric(opt.network_id, debug=opt.debug)
         print(ret)
     else:
         print("ERROR: specify the network id")
